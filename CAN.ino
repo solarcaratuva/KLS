a9ac2a802e7848658d36093f09b5f60d3261ea11
William Zhang <willzhang05@gmail.com>
2019-03-22 17:32:06 -0400

    some cleanup

--- CAN.ino
+++ CAN.ino
@@ -1,10 +1,8 @@
-// CAN Blaster 9000 will transmit packets over the can bus periodically
-
-//#include <WProgram.h>
-//#include <ArduinoJson.h>  // uses v5 API, version 5.13.4
-#include <FlexCAN.h>  // This one is the one that was built-in to arduino
-#include <Metro.h>    // This is basically the Timer library I pasted in #ElectricalTeam
+// Uses collin80 library found at
+// https://github.com/collin80/FlexCAN_Library
 #include <kinetis_flexcan.h>
+#include <FlexCAN.h>
+#include <Metro.h>
 #include <cstdlib>
 #include <string>
 
@@ -14,12 +12,12 @@ void parse_serial();
 
 void setup() {
     Can1.begin();
-    msg_rx.flags.extended = 1;
-    msg_tx.id = 0x100;          // ID field
-    msg_tx.flags.extended = 1;  // extended identifier
-    msg_tx.len = 8;             // number of bytes to expect in data field
-    strncpy((char *)msg_tx.buf, "hello",
-            8);  // data field (strnpcy used to prevent copy of null terminator)
+    msg_rx.flags.extended = 1; 
+    msg_tx.id = 0x100;  // ID field
+    msg_tx.flags.extended = 1;     // extended identifier
+    msg_tx.len = 8;     // number of bytes to expect in data field
+    // data field (strnpcy used to prevent copy of null terminator)
+    strncpy((char *)msg_tx.buf, "hello", 8); 
     while (!Serial) continue;
     Serial.println(F("CAN Blaster 9000!"));
 }
@@ -34,8 +32,8 @@ void loop() {
 }
 
 static void print_CAN_msg(CAN_message_t &msg) {
-    static bool print_header =
-        true;  // static prefix makes variable persistent with each call of this function.
+    // static prefix makes variable persistent with each call of this function.
+    static bool print_header = true;  
     if (print_header) {
         Serial.println(F("id,\timestamp,\tlen,\text,\t[buf]"));
         print_header = false;
@@ -61,7 +59,7 @@ static void print_CAN_msg(CAN_message_t &msg) {
 
 // really shitty serial interpreter.
 // works for the momenet but uses
-// nieve branching logic structures.
+// naive branching logic structures.
 // should be implemented more
 // abstractly with data structures
 // like classes or nested structures
@@ -71,8 +69,7 @@ void parse_serial() {
         char c = Serial.read();
         static char tokens[10][16] = {"", "", "", "", "", "", "", "", "", ""};
         static uint8_t tokenIndex = 0;
-        static uint8_t i =
-            0;  // represents index of character in a given token (defined by tokenIndex)
+        static uint8_t i = 0;  // represents index of character in a given token (defined by tokenIndex)
         uint8_t parse_i = 0;  // once the '\n' character is received, the tokens are decoded and
                               // branching logic is used to read/write system variables.
         // buffer overflow prevention
