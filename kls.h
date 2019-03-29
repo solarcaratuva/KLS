d784d7f51dbaf119cf946561124517525315fffe
William Zhang <willzhang05@gmail.com>
2019-03-29 19:48:19 -0400

    parsed message2

--- kls.h
+++ kls.h
@@ -74,16 +74,16 @@ void print_msg_decode(CAN_message_t &msg) {
 
         unsigned int curr_lsb = msg.buf[2];
         unsigned int curr_msb = msg.buf[3] << 8;
-        unsigned int curr = (curr_msb + curr_lsb);
+        float curr = (curr_msb + curr_lsb) * 1000.0;
         Serial.print("Current: ");
-        Serial.print(curr, DEC);
-        Serial.println(" A");
+        Serial.print(curr, 3);
+        Serial.println(" mA");
 
         unsigned int volt_lsb = msg.buf[4];
         unsigned int volt_msb = msg.buf[5] << 8;
-        unsigned int volt = (volt_msb + volt_lsb) / 10;
+        float volt = (volt_msb + volt_lsb) / 10.0;
         Serial.print("Voltage: ");
-        Serial.print(volt, DEC);
+        Serial.print(volt, 3);
         Serial.println(" V");
 
         unsigned int err_lsb = msg.buf[6];
@@ -102,9 +102,73 @@ void print_msg_decode(CAN_message_t &msg) {
             }
             err_msb >>= 1;
         }
+        Serial.println("---");
     }
     // message 2
     if (msg.id == 0x0CF11F05) {
+        float throttle = (msg.buf[0] * 5) / 255.0;
+        Serial.print("Throttle: ");
+        Serial.print(throttle, 3);
+        Serial.println(" V");
+
+        unsigned int cont_temp = msg.buf[1] - 40;
+        Serial.print("Controller Temp: ");
+        Serial.print(cont_temp);
+        Serial.println(" C");
+
+        unsigned int motor_temp = msg.buf[2] - 30;
+        Serial.print("Motor Temp: ");
+        Serial.print(motor_temp);
+        Serial.println(" C");
+
+        unsigned int cont_status = msg.buf[4];
+
+        // two least significant bits
+        unsigned int comm_status = cont_status & 0x03;
+
+        Serial.print("Command status: ");
+        Serial.println(comm_status);
+
+        unsigned int fdbk_status = (cont_status & 0x0C) >> 2;
+
+        Serial.print("Feedback status: ");
+        Serial.println(fdbk_status);
+
+        unsigned int switch_status = msg.buf[5];
+        
+        bool hall_a = switch_status & 0x01;
+        bool hall_b = switch_status & 0x02;
+        bool hall_c = switch_status & 0x04;
+        
+        Serial.print("Hall sensors: ");
+        Serial.print(hall_a);
+        Serial.print(" ");
+        Serial.print(hall_b);
+        Serial.print(" ");
+        Serial.println(hall_c);
+        
+        bool brake = switch_status & 0x08;
+        bool backward = switch_status & 0x10;
+        bool forward = switch_status & 0x20;
+        bool foot = switch_status & 0x40;
+        bool boost = switch_status & 0x80;
+
+        Serial.print("Brake: ");
+        Serial.println(brake);
+
+        Serial.print("Backward: ");
+        Serial.println(backward);
+
+        Serial.print("Forward: ");
+        Serial.println(forward);
+
+        Serial.print("Foot: ");
+        Serial.println(foot);
+
+        Serial.print("Boost: ");
+        Serial.println(boost);
+
+        Serial.println("---");
     }
 }
 
