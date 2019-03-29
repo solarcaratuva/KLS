4225e31ecd0c7eaf288db7a13fa4f0943be6b1a0
William Zhang <willzhang05@gmail.com>
2019-03-29 18:33:03 -0400

    compileable print decode

--- kls.h
+++ kls.h
@@ -68,17 +68,23 @@ void print_msg_decode(CAN_message_t &msg) {
         unsigned int speed_lsb = msg.buf[0];
         unsigned int speed_msb = msg.buf[1] << 8;
         unsigned int speed = speed_msb + speed_lsb;
-        Serial.println("Motor speed: %d RPM", speed);
+        Serial.print("Speed: ");
+        Serial.print(speed, DEC);
+        Serial.println(" RPM");
 
         unsigned int curr_lsb = msg.buf[2];
         unsigned int curr_msb = msg.buf[3] << 8;
         unsigned int curr = (curr_msb + curr_lsb) / 10;
-        Serial.println("Motor current: %d A", curr);
+        Serial.print("Current: ");
+        Serial.print(curr, DEC);
+        Serial.println(" A");
 
         unsigned int volt_lsb = msg.buf[4];
         unsigned int volt_msb = msg.buf[5] << 8;
         unsigned int volt = (volt_msb + volt_lsb) / 10;
-        Serial.println("Motor voltage: %d V", volt);
+        Serial.print("Voltage: ");
+        Serial.print(volt, DEC);
+        Serial.println(" V");
     }
 
     if (msg.id == 0x0CF11F05) {
